8f225925c038fd8308f8ee7ba2e0ef855942c654
aayushmnit <aayushmnit@gmail.com>
2016-11-28 12:29:40 -0600

    Changes to movie files

--- code/3_omdb_pull.R
+++ code/3_omdb_pull.R
@@ -1,18 +1,17 @@
 #devtools::install_github("hrbrmstr/omdbapi")
 library(omdbapi)
 library(dplyr)
+library(stringr)
 setwd("C:/Users/Aayush - Carlson/Desktop/PA/")
 
 movies <- read.csv("movies.csv")
+movies$movie_id <- str_pad(movies$movie_id, 7, pad = "0")
 movies$imdb_id <- paste('tt',movies$movie_id,sep ="")
 
 movie_omdb <- find_by_id(movies[1,'imdb_id'])
-for (i in 12162:nrow(movies)){
+for (i in 1:nrow(movies)){
   movie_omdb <- rbind(movie_omdb,find_by_id(movies[i,'imdb_id']))
   print(i)
 }
-x <- find_by_id(movies[10257,'imdb_id'])[,colnames(movie_omdb)]
-movie_omdb <- rbind(movie_omdb,x)
-#10030
-#10257
+
 write.csv(movie_omdb,"movie_omdb.csv",row.names = FALSE)

288ba53c6902c4dca786f0580f6eeb9d99e70a08
Aayush Agrawal <aayushmnit@gmail.com>
2016-12-03 16:27:26 -0600

    Update 1_R code for importing.R

--- code/1_R code for importing.R
+++ code/1_R code for importing.R
@@ -1,8 +1,8 @@
 library(data.table)
-setwd("C:/Users/Aayush - Carlson/Desktop/PA/")
+#setwd("C:/Users/Aayush - Carlson/Desktop/PA/")
 
 ## Movie Table
-lines = readLines ('movies.txt')
+lines = readLines ('./inputdata/movies.txt')
 movies <- data.frame(movie_id = character(0), movie_title = character(0), genre = character(0))
 movies$movie_id <- as.character(movies$movie_id)
 movies$movie_title <- as.character(movies$movie_title)
@@ -15,17 +15,17 @@ for (i in 1:length(lines)){
   movies[i,"genre"] <- y[[1]][3]
 }
 
-write.csv(movies,"movies.csv",row.names = FALSE)
+write.csv(movies,"./output/movies.csv",row.names = FALSE)
 
 #User Table
-x <- readLines("users.txt")
+x <- readLines("./inputdata/users.txt")
 x <- gsub("::", "*", x)
 users = read.table(text=x, sep="*", fill=TRUE,col.names = c('user_id','twitter_id'))
-write.csv(users,"users.csv",row.names = FALSE)
+write.csv(users,"./outputdata/users.csv",row.names = FALSE)
 
 #Ratings Table
-ratings <- fread("ratings.txt",":")
+ratings <- fread("./inputdata/ratings.txt",":")
 ratings <- ratings[,.(V1,V3,V5,V7)]
 colnames(ratings) <- c("user_id","movie_id","rating","rating_timestamp")
 ratings$rating_timestamp <- as.POSIXct(ratings$rating_timestamp, origin="1970-01-01")
-write.csv(ratings,"ratings.csv",row.names = FALSE)
+write.csv(ratings,"./outputdata/ratings.csv",row.names = FALSE)
